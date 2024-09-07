834a1ae9139e7d908a69b279bf1330c903953190
sahil verma <100288753+Romian11@users.noreply.github.com>
2024-09-08 02:11:27 +0530

    Update blog.js
    asda

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 import { getFirestore, collection, doc , getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // Your web app's Firebase configuration
 // TODO: Add SDKs for Firebase products that you want to use
